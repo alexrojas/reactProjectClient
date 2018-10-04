296b21b72f9b8661276586d4b5d982c52ca49076
alexrojas <alex.rojas@agintechllc.com>
2018-10-04 11:51:31 -0400

    added bootstrap, fontawesome, created forms, setting, setting up the register form with Its component state

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4"> Tesla Private Network
+                </h1>
+                <p className="lead">
+                  The purpose of the club is to have fun, meet, exchange information, educate, and share charging stations with other Tesla owners.</p>
+                <hr/>
+
+                  <Link className="btn btn-lg btn-info mr-2" to={'/register'}>Sign Up</Link>
+
+                  <Link className="btn btn-lg btn-light" to={'/login'}>Login</Link>
+    
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>)
+  }
+}
+
+export default Landing
