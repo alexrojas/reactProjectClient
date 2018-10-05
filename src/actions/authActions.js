ac82656c44c83445d666236b8c3b88c322ff5055
alexrojas <alex.rojas@agintechllc.com>
2018-10-04 22:01:46 -0400

    added the login action and set the current user, storing the token on localstorage

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,5 +1,9 @@
 import {GET_ERRORS} from './types'
+import {SET_CURRENT_USER} from './types'
+import setAuthToken from '../utils/setAuthToken'
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
 
 export const registerUser = (userData, history) => dispatch  => {
   axios.post('/api/users/register', userData)
@@ -13,4 +17,33 @@ export const registerUser = (userData, history) => dispatch  => {
       payload: err.response.data
     })
   )
+}
+
+
+// login user token
+export const loginUser = userData => dispatch  => {
+  axios.post('/api/users/login', userData)
+  .then((res)=>{
+    //save ot local storage
+    const token = res.data.token
+    //set token to local storage
+    localStorage.setItem('jwtToken', token)
+    // set token to auth header
+    setAuthToken(token)
+    const decoded = jwt_decode(token)
+    dispatch(setCurrentUser(decoded))
+  })
+  .catch(err =>
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  )
+}
+
+export const setCurrentUser = decoded => {
+  return{
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
 }
